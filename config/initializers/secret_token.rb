013e03d9d46a22f588f08e7f525dbd9b7b23ecf4
Wang Song <song.wang@brilliantech-solution.com>
2013-11-21 13:41:45 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goschool::Application.config.secret_token = '5838d19f92c8c8d267dc05ee1f382a89cccd3627dfd66db057208687726298e5b22ecbf57b187c9f0f91bf7768d325a4551cfef3b83591b58dbc3d252d56c90c'
