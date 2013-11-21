d7e60e526747ed8a9cc15e79214236348b9a9bf3
Tianyi Shen <tianyi.shen@cz-tek.com>
2013-11-21 16:24:19 +0800

    API for tag,recommendation and search engine add tenantid to recommendation

--- lib/recommendation.rb
+++ lib/recommendation.rb
@@ -7,14 +7,14 @@ class Recommendation
   # @param [Integer] student_id
   # @return [Array] a array contains all the potential relationship of the
   # target student
-  def get_potential_relation(student_id)
+  def get_potential_relation(tenant_id,student_id)
 
   end
 
 
   # @param [String] course_id
   # @return [Array] A int array contains all the recommended customers id
-  def get_course_potential_customers(course_id)
+  def get_course_potential_customers(tenant_id,course_id)
 
   end
 
