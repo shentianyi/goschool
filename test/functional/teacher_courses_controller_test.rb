8703ae77d10e11b76632371b3d917d2bb3294569
Wang Song <song.wang@brilliantech-solution.com>
2013-11-25 15:31:33 +0800

    add teacher course teste

--- test/functional/teacher_courses_controller_test.rb
+++ test/functional/teacher_courses_controller_test.rb
@@ -4,46 +4,26 @@ class TeacherCoursesControllerTest < ActionController::TestCase
   setup do
     @teacher_course = teacher_courses(:one)
   end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:teacher_courses)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+ 
 
   test "should create teacher_course" do
     assert_difference('TeacherCourse.count') do
-      post :create, teacher_course: {  }
+      post :create, teacher_course: {sub_course_id:sub_courses(:one).id,user_id:users(:one)  }
+      puts response.body
     end
-
-    assert_redirected_to teacher_course_path(assigns(:teacher_course))
+ 
   end
-
-  test "should show teacher_course" do
-    get :show, id: @teacher_course
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @teacher_course
-    assert_response :success
-  end
-
+ 
   test "should update teacher_course" do
-    put :update, id: @teacher_course, teacher_course: {  }
-    assert_redirected_to teacher_course_path(assigns(:teacher_course))
+    put :update, id: @teacher_course, teacher_course: {sub_course_id:sub_courses(:two),user_id:users(:one)  }
+    puts response.body
   end
 
   test "should destroy teacher_course" do
     assert_difference('TeacherCourse.count', -1) do
       delete :destroy, id: @teacher_course
+      puts response.body
     end
-
-    assert_redirected_to teacher_courses_path
+ 
   end
 end
--- test/unit/helpers/cources_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CourcesHelperTest < ActionView::TestCase
-end
