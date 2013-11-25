d80905d7e6bd8707a20251366af7a5564d24ed18
Wang Song <song.wang@brilliantech-solution.com>
2013-11-25 14:53:56 +0800

    add finish teacher course

--- app/controllers/teacher_courses_controller.rb
+++ app/controllers/teacher_courses_controller.rb
@@ -3,29 +3,36 @@ class TeacherCoursesController < ApplicationController
   before_filter :init_message ,:only=>[:create,:update,:destroy]
   before_filter :get_teacher_course , :only=>[:update,:show,:destroy]
   before_filter :render_nil_msg , :only=>[:update,:destroy]
+  
   def create
     @teacher_course = TeacherCourse.new(params[:teacher_course])
-
+    unless @msg.result=@teacher_course.save
+     @msg.content=@teacher_course.errors.messages
+    end
+    render :json=>@msg
   end
 
   def update
-    @teacher_course = TeacherCourse.find(params[:id])
-
+    unless @msg.result=@teacher_course.update_attributes(params[:teacher_course])
+     @msg.content=@teacher_course.errors.messages
+    end
+    render :json=>@msg
   end
 
   def destroy
-    @teacher_course = TeacherCourse.find(params[:id])
     @teacher_course.destroy
+    @msg.result=true
+    render :json=>@msg
   end
 
   private
 
   def get_teacher_course
-    @course=Course.find_by_id(params[:id])
+    @teacher_course=TeacherCourse.find_by_id(params[:id])
   end
 
   def render_nil_msg
-    unless @course
+    unless @teacher_course
       @msg.content='不存在此课程'
       render :json=>msg
     end
