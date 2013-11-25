b0214f9c308743ca54f3494e0103d999cd424f9f
wayne <zixiao.wang@cz-tek.com>
2013-11-25 18:22:04 +0800

    student list

--- app/assets/javascripts/BACK_index.js
+++ app/assets/javascripts/BACK_index.js
@@ -7,4 +7,28 @@ BACKINDEX.init=(function(){
     $("body").on("click","#view-add-close",function(){
         $(".view-add").css("display","none");
     });
+    $("#add-item").on("click",function(event){
+        stop_propagation(event);
+        if($(this).attr("state")=="close" || $(this).attr("state")==null){
+            $("#back-index-add").css("right","0");
+            $("#back-index-main").css("right","250px");
+            $(this).attr("state","open");
+        }
+        else{
+            $("#back-index-add").css("right","-260px");
+            $("#back-index-main").css("right","0px");
+            $(this).attr("state","close");
+        }
+    });
+    $("body").on("click",function(event){
+       var target=adapt_event(event).target;
+       if(target!=document.getElementById("back-index-add")){
+           $("#back-index-add").css("right","-260px");
+           $("#back-index-main").css("right","0px");
+           $("#add-item").attr("state","close");
+       }
+    });
+    $("#back-index-add").on("click","input",function(event){
+        stop_propagation(event);
+    });
 })();
---
+++ app/assets/javascripts/BACK_rightList.js
@@ -0,0 +1,118 @@
+//type name
+//should be
+//student teacher course
+
+var BACKINDEX=BACKINDEX || {};
+BACKINDEX.right_list=BACKINDEX.right_list||{};
+BACKINDEX.right_list.student={};
+BACKINDEX.right_list.teacher={};
+BACKINDEX.right_list.course={};
+BACKINDEX.right_list.student.info={
+     href:""
+};
+BACKINDEX.right_list.initital=(function(){
+    BACKINDEX.right_list.type=$("#back-index-main").attr("name");
+    $(document).ready(function(){
+        $("#back-index-main").scroll(BACKINDEX.right_list.observeScroll)
+    });
+})();
+//////////////////////////////////////////////////////////////////////////////////////////////////// 列表的呈现
+BACKINDEX.right_list.currentPage;
+BACKINDEX.right_list.loadCheck=0;
+BACKINDEX.right_list.stillHave=true;
+BACKINDEX.right_list.threshold=50;
+BACKINDEX.right_list.id;
+BACKINDEX.right_list.generateResult=function(parameter){
+    BACKINDEX.right_list.id=parameter;
+    BACKINDEX.right_list.currentPage=0;
+    BACKINDEX.right_list.loadData();
+}
+BACKINDEX.right_list.loadData=function(){
+    if(BACKINDEX.right_list.loadCheck!=0) return;
+    BACKINDEX.right_list.loadCheck++;
+    loader("search-result","auto","auto","0","50%");
+    window.setTimeout(function(){
+        $.getJSON(BACKINDEX.right_list.nextPage(),{},function(data){
+            remove_loader();
+            if(data.length==0){
+                BACKINDEX.right_list.stillHave=false;
+                return
+            }
+            var render_data={},type=BACKINDEX.right_list.type;
+            render_data[type]=data;
+            var render=Mustache.render(BACKINDEX.right_list[type].template,render_data);
+            $("#search-result").append(render);
+        }).always(function(){
+                BACKINDEX.right_list.loadCheck--;
+        });
+        window.setTimeout(function(){
+            if($("#search-list").height()+parseInt($("#search-list>.search-input").css("margin-top"))<$(window).height()){
+                BACKINDEX.right_list.loadData();
+            }
+        },100);
+    },1000);
+};
+BACKINDEX.right_list.nextPage=function(){
+    BACKINDEX.right_list.currentPage++;
+    var type=BACKINDEX.right_list.type;
+    var newURL=BACKINDEX.right_list[type].info.href+BACKINDEX.right_list.id+"/"+BACKINDEX.right_list.currentPage;
+    return newURL;
+};
+BACKINDEX.right_list.observeScroll=function(){
+   if($("#loader").is(":visible")) return;
+   var bottomPosition=$(window).scrollTop()+$(window).height();
+   var distanceFromBottom=$(document).height()-bottomPosition;
+   if(distanceFromBottom<=BACKINDEX.right_list.threshold && $("#search-result").size()>0 && BACKINDEX.right_list.stillHave){
+       BACKINDEX.right_list.loadData();
+   }
+};
+BACKINDEX.right_list.student.template="\
+    {{#student}}\
+    <li>\
+    <table class='ui table segment'>\
+        <tr>\
+            <td class='two wide'>\
+                <img class='circular ui image mini portrait' src='images/portrait/0.jpg' />\
+                <p class='name'>{{name}}</p>\
+            </td>\
+            <td class='two wide'>\
+                <p class='type'>{{type}}</p>\
+                <p>{{school}}</p>\
+                <p>{{grade}}</p>\
+                <p>{{age}}岁</p>\
+            </td>\
+            <td class='five wide'>\
+                <div class='ui blue labels small'>\
+                    {{#tag}}\
+                    <a class='ui label'>\
+                      {{title}}\
+                    </a>\
+                    {{/tag}}\
+                </div>\
+                <p class='parent'><span>监护人:</span><span>{{parent}}</span></p>\
+                <p class='phone'><span>联系电话:</span><span>{{phone}}</span></p>\
+                <p class='consult-time'><span>上次咨询:</span><span>{{consult-time}}</span></p>\
+            </td>\
+            <td class='two wide'>\
+                <p class='maybe-know'>推荐人</p>\
+                <p>{{recommend}}</p>\
+            </td>\
+            <td class='three wide'>\
+                <p class='recommend'>可能认识</p>\
+                <p>\
+                   {{#may_know}}\
+                   <span>{{name}}</span>\
+                   {{/may_know}}\
+                </p>\
+            </td>\
+            <td class='three wide'>\
+                <div class='ui mini button teal'>\
+                详情\
+                </div>\
+            </td>\
+        </tr>\
+    </table>\
+    </li>\
+{{/student}}"
+//////////////////////////////////////////////////////////////////////////////////////////////////// 添加
+
