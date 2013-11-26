38c358c1192c0d2a595f485d8ec37d8e6094583d
Charlot <7wangsong@gmail.com>
2013-11-26 18:15:38 +0800

    fix setting

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,5 @@
 #encoding: utf-8
 class SettingsController < ApplicationController
-  before_filter :get_setting,:only=>[:update]
   def index
     @institutions= current_tenant.institutions
   end
@@ -19,15 +18,10 @@ class SettingsController < ApplicationController
 
   def update
     msg=Msg.new
+    @setting=current_tenant.setting
     unless msg.result=@setting.update_attributes(params[:setting].strip)
     msg.content=@setting.errors.messages
     end
     render :json=>msg
-  end
-
-  private
-
-  def get_setting
-    @setting=current_tenant.setting
   end
 end
