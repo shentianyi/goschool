e7e5511400da9bba47f0cc65a1051503acf70a23
tesla lee <leechee89@gmail.com>
2013-11-27 15:28:20 +0800

    finish student controller

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
+#encoding: utf-8
 class Student < ActiveRecord::Base
+  include Redis::Search
+
   attr_accessible :address, :birthday, :email, :gender, :graduation, :guardian, :guardian_phone, :name, :phone, :school,:referrer_id,:logininfo_id,:image_url,:tenant_id
   
   belongs_to :logininfo
@@ -6,4 +9,14 @@ class Student < ActiveRecord::Base
 
   has_one :logininfo_role, :through=>:logininfo
   belongs_to :referrer, :foreign_key => :referrer_id, :class_name => "Logininfo"
+
+  acts_as_tenant(:tenant)
+
+  redis_search_index(:title_field =>:name,
+                     :condition_field => [:tenant_id],
+                     :ext_fields=>[:email,:address,:school,:guardian])
+
+  def add_tags tags
+    Resque.enqueue(TagAdder,self.tenant_id,self.class.name,self.id,tags)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Goschool::Application.routes.draw do
     get :courses
    end
   end
-  resources :students
+  resources :students do
+    collection do
+      get :list_search
+    end
+  end
 
   resources :teacher_courses
 
@@ -36,6 +40,7 @@ Goschool::Application.routes.draw do
   mount Resque::Server.new, :at=>"/admin/resque"
 
   resources :users
+
   resources :logininfos
   resource :subscriptions
   resource :logininfo_session
