69a751062d9183eae128afeb475fec5ec733050e
Charlot <7wangsong@gmail.com>
2013-11-27 16:20:59 +0800

    update student course presenter add delegators

--- app/presenters/presenter.rb
+++ app/presenters/presenter.rb
@@ -1,9 +1,9 @@
 #encoding: utf-8
 class Presenter
   extend Forwardable
-  def initialize(params)
-    params.each_pair do |attr,value|
-      self.send :"#{attr}=",value
-    end unless params.nil?
-  end
+  # def initialize(params)
+    # params.each_pair do |attr,value|
+      # self.send :"#{attr}=",value
+    # end unless params.nil?
+  # end
 end
