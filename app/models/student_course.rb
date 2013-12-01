a95d63a4fd39f36205e235476fd8fc30904d5c81
Charlot <7wangsong@gmail.com>
2013-12-02 00:35:22 +0800

    add student course callback to update student status

--- app/models/student_course.rb
+++ app/models/student_course.rb
@@ -7,8 +7,8 @@ class StudentCourse < ActiveRecord::Base
   attr_accessible :student_id,:course_id
   validate :validate_save
   acts_as_tenant(:tenant)
-  after_create :incr_course_actual_number
-  after_destroy :descr_couse_actual_number
+  after_create :incr_course_stu_status
+  after_destroy :decr_couse_stu_status
 
   private
   def validate_save
@@ -16,11 +16,15 @@ class StudentCourse < ActiveRecord::Base
     errors.add(:user_id,'学生已报名此课程') if self.class.where(:student_id=>self.student_id,:course_id=>self.course_id).where('id<>?',self.id).first unless new_record?
   end
 
-  def incr_course_actual_number
+  def incr_course_stu_status
     self.course.increment!(:actual_number)
+    # self.student.update_attrubutes(:status=>KEHU)
   end
 
-  def descr_couse_actual_number
+  def decr_couse_stu_status
    self.course.decrement!(:actual_number)
+   # if student=self.student.courses.count==0
+     # student.update_attributes(:status=>QIAN_ZAI_KE_HU)
+   # end
   end
 end
