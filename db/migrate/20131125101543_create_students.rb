011981669624de8637516c90b31c06d931d47176
tesla lee <leechee89@gmail.com>
2013-12-02 11:05:48 +0800

    sds

--- db/migrate/20131125101543_create_students.rb
+++ db/migrate/20131125101543_create_students.rb
@@ -12,7 +12,7 @@ class CreateStudents < ActiveRecord::Migration
       t.string :guardian
       t.string :guardian_phone
       t.string :image_url
-      t.boolean :is_potential_cus
+      t.boolean :is_potential_cus :default=>false
       t.references :logininfo
       t.integer :referrer_id
       t.references :tenant
