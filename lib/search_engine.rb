d7105d9b6734ff74b041b07665c6dfa178ad7b45
Tianyi Shen <tianyi.shen@cz-tek.com>
2013-12-04 10:45:24 +0800

    search public api

--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -1,17 +1,28 @@
 class SearchEngine
-
-  # @param [String] search_string, a processed search pattern string
-  # @return [Array] of the queried object (course, Student)
-  def search(search_string)
+  # @param [String] search_queries, a processed search conditions hash
+  # @return [ACTIVERECORD::Relation] of the queried object (course, Student)
+  def search_return_relation(search_queries)
 
 
   end
 
-# @param [String] search_string, a processed search pattern string
+
+
+# @param [String] search_queries, a processed search conditions hash
 # @param [Integer] take, the number of records to return
 # @param [Integer] from, the index of records to start. Start from 0
 # @return [Array] of the queried object (course, Student)
-  def search(search_string,take,from)
+  def search(search_queries,take=-1,from=0)
+
+  end
+
+
+  def get_query_type(query_type_key)
+
+  end
+
+  def get_all_query_types(entity_type=nil)
+
 
   end
 
