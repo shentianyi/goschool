871765229ae97437075cbcef76edc84a7225fe56
Charlot <7wangsong@gmail.com>
2013-12-04 17:51:53 +0800

    add course schedule service

--- app/services/CourseScheduleService.rb
+++
@@ -1,9 +0,0 @@
-#encoding: utf-8
-class CourseScheduleService
- def generate_employee_schedule_by_institution institution_id
-   if Schedule.count_by_institution_id(institution_id)>0
-     emails=User.get_employees(institution_id).collect{|user| user.email}
-     schedules=Schedule.by_insititution_id(institution_id)
-   end
- end
-end
