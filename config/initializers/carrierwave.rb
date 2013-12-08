ee80fd3d97d5b586a9126d4781c23461f772eacb
Charlot <7wangsong@gmail.com>
2013-12-08 19:38:30 +0800

    add carrierwave conf

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,7 @@
+CarrierWave.configure do |config|
+  config.storage = :aliyun
+  config.aliyun_access_id = "VDXgx2g1F7gZe8SY"
+  config.aliyun_access_key = 'cVJoHKSh377Al7il90VuQD7Fo1GiPG'
+  config.aliyun_bucket = "goschool-schedule"
+  config.aliyun_internal = true
+end
