112d76ecbb04eaaae8e2889d96962c8497040786
Charlot <7wangsong@gmail.com>
2013-12-09 15:05:06 +0800

    send schedule to all

--- app/services/email_service.rb
+++ app/services/email_service.rb
@@ -2,6 +2,10 @@
 class EmailService
   def self.send_schedule_email type,institution_id
     case type
+    when ScheduleType::ALL
+      send_employee_schedule_email ScheduleType::EMPLOYEE,institution_id
+      send_teacher_schedule_email ScheduleType::TEACHER,institution_id
+      send_student_schedule_email ScheduleType::STUDENT,institution_id
     when ScheduleType::EMPLOYEE
       send_employee_schedule_email type,institution_id
     when ScheduleType::TEACHER
@@ -24,28 +28,29 @@ class EmailService
   def self.send_teacher_schedule_email type,institution_id
     teachers=User.get_teachers(institution_id)
     teachers.each do |teacher|
-     css=CourseScheduleService.new({type:type,institution_id:institution_id,user_id:teacher.id})
-     if schedules=css.generate_schedule
-       send_schedule schedules,teacher.email
-     end
+      css=CourseScheduleService.new({type:type,institution_id:institution_id,user_id:teacher.id})
+      if schedules=css.generate_schedule
+        send_schedule schedules,teacher.email
+      end
     end
   end
 
   def self.send_student_schedule_email type,institution_id
-   Course.where('actual_number>0').all.each do |course|
-    css=CourseScheduleService.new({type:type,institution_id:institution_id,course_id:course.id})
-    if schedules=css.generate_schedule
-      emails=course.students.pluck(:email)
-      send_schedule schedules,emails
+    Course.where('actual_number>0').all.each do |course|
+      css=CourseScheduleService.new({type:type,institution_id:institution_id,course_id:course.id})
+      if schedules=css.generate_schedule
+        emails=course.students.pluck(:email)
+        send_schedule schedules,emails
+      end
     end
-   end
   end
 
   private
+
   def self.send_schedule schedules,emails
-      file_name=SecureRandom.uuid+'.pdf'
-      schedule_pdf=PdfService.generate_schedule_pdf(schedules)
-      url=AliyunOssService.store_schedule(file_name,schedule_pdf)
-      ScheduleMailer.download_notify(emails,url).deliver
+    file_name=SecureRandom.uuid+'.pdf'
+    schedule_pdf=PdfService.generate_schedule_pdf(schedules)
+    url=AliyunOssService.store_schedule(file_name,schedule_pdf)
+    ScheduleMailer.download_notify(emails,url).deliver
   end
 end
