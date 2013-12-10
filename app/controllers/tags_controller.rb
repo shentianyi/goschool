c8a0204fcd7e6959892111d1ad09fe90f9d347e8
Charlot <7wangsong@gmail.com>
2013-12-10 14:22:25 +0800

    add tags controller

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,6 @@
+#encoding: utf-8
+class TagsController < ApplicationController
+  def fast_search
+    render json: TagUtility.new.fast_search(params[:q],10,current_tenant.id).map{|tag| {name:tag}}
+  end
+end
