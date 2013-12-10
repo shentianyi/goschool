f0dcdfa40cf9b15a2782ef8d6c2d81956bb767a7
wayne <zixiao.wang@cz-tek.com>
2013-12-10 11:04:22 +0800

    course index

---
+++ app/assets/javascripts/FRONT_index.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: wayne
+ * Date: 13-12-10
+ * Time: 上午10:20
+ * To change this template use File | Settings | File Templates.
+ */
+(function(){
+    $("#main-navigator-bottom i").popup();
+})()
