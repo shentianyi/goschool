f2295554ca66d05499e5cdee07fdafae6a513be8
Charlot <7wangsong@gmail.com>
2013-12-11 11:43:31 +0800

    course search index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class TagsController < ApplicationController
   def fast_search
-    render json: TagUtility.new.fast_search(params[:q],10,current_tenant.id).map{|tag| {name:tag}}
+    render json: TagUtility.new.fast_search(params[:q],10,current_tenant.id).map{|tag| {name:tag['title']}}
     # render json:[{name:'jack'}]
   end
 end
