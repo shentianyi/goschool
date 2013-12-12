0e03d050bd42581ec8f265dee517e7b931f5189f
tesla lee <leechee89@gmail.com>
2013-12-12 10:35:25 +0800

    require admin

--- app/observers/student_observer.rb
+++ app/observers/student_observer.rb
@@ -3,6 +3,6 @@ class StudnetObserver<ActiveRecord::Observer
   observe :student
   
   def after_save student
-    TagService.add_tags(student)
+    TagService.add_tags(student) if student.tags
   end
 end
