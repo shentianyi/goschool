f37565da0ef94f442731b89d8f569b2d9ed588d4
Charlot <7wangsong@gmail.com>
2013-12-12 17:06:28 +0800

    add course show students

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -126,7 +126,7 @@ class CoursesController < ApplicationController
   end
 
   def get_course
-    @course=Course.find_by_id(params[:id])
+    @course=Course.joins(:institution).where(id:params[:id]).select('courses.*,institutions.name as institution_name').first
   end
 
   def render_nil_msg
