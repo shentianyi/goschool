ffc59a609e71d8fecf9b4e0873f3594fd9fc1172
tesla lee <leechee89@gmail.com>
2013-12-12 20:16:05 +0800

    students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -31,7 +31,7 @@ class StudentsController < ApplicationController
       consultation(@student)
     end
     
-    @partia ||= params[:part]
+    @partial ||= params[:part]
     render :partial=>@partial if params[:ajax]
 
     #respond_to do |formxat|
@@ -40,30 +40,7 @@ class StudentsController < ApplicationController
     #end
   end
 
-  def courses(student)
-    
-  end
-
-  def achievements(student)
-    
-  end
-
-  def relation(student)
-    @referrer = Logininfo.find_by_id(student.referrer_id)
-    @students = []
-    relations = []
-    relations = Recommendation.new.get_potential_relation(student.tenant_id,student.id)
-    relations.each do |relation|
-      s = Student.find_by_id(relation['id'])
-      if s
-        @students<<s
-      end
-    end
-  end
-
-  def consultation(student)
-    @consultations = StudentConsultationPresenter.init_presenters(@studnet.consultations)
-  end
+  
 
   # GET /students/new
   # GET /students/new.json
@@ -160,11 +137,6 @@ class StudentsController < ApplicationController
     end
     render :json=>students
   end
-
-  #get student
-  def get_student
-    @student=Student.find_by_id(params[:id])
-  end
   
   #nil msg
   def render_all_msg
@@ -174,29 +146,30 @@ class StudentsController < ApplicationController
     end
   end
 
-  #create consulting record
-  def add_consultation
-    msg = Msg.new
-    msg.result = false
-    @student = Student.find_by_id(params[:id])
-    if @student
-      @consultation = Consultation.new(params[:consultation])
-      @consultation.logininfo = current_user
-      student.consultations<<@consultation
-      student.save!
-      msg.result = true
-    else
-      msg.content = '学生不存在'
+  private
+  
+  def courses(student)
+    
+  end
+  
+  def achievements(student)
+    
+  end
+
+  def relation(student)
+    if student.referrer_id
+      @referrer = Logininfo.find(student.referrer_id).student
+    end
+    @students = []
+    Recommendation.new.get_potential_relation(student.tenant_id,student.id).each do |relation|
+      s = Student.find_by_id(relation['id'])
+      if s
+        @students<<s
+      end
     end
   end
 
-  #search
-  def search
-    msg = Msg.new
-    msg.result = false
-    @results = SearchEngine.search(params[:query_string])
-    msg.result = false
-    msg.content = @results
-    render :json=>msg
+  def consultation(student)
+    @consultations = StudentConsultationPresenter.new.init_presenters(@studnet.consultations)
   end
 end
