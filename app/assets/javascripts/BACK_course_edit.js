d865f0f48ec2b6ec8a99251394721eab67d920f2
Charlot <7wangsong@gmail.com>
2013-12-13 12:59:15 +0800

    rewrite js

--- app/assets/javascripts/BACK_course_edit.js
+++ app/assets/javascripts/BACK_course_edit.js
@@ -1,16 +1,17 @@
-var course_manager = {
-	source : 'courses',
-	edit : function(id, callback) {
-		manager.edit(this.source, id, callback);
-	},
-	update : function(id, data, callback) {
-		manager.update(this.source, id, data, callback);
-	}
-}
+var course_manager = $.extend({
+     source : 'courses'
 
-var teacher_course_manager = {
-	source : 'teacher_courses',
-	destroy : function(id, callback) {
-		manager.destroy(this.source, id, callback);
-	}
-}
+     // edit : function(id, callback) {
+     // manager.edit(this.source, id, callback);
+     // },
+     // update : function(id, data, callback) {
+     // manager.update(this.source, id, data, callback);
+     // }
+}, manager);
+
+var teacher_course_manager = $.extend({
+     source : 'teacher_courses'
+     // destroy : function(id, callback) {
+          // manager.destroy(this.source, id, callback);
+     // }
+}, manager);
