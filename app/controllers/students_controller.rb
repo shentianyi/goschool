eebec791ee5c45401a9a4467831a7fc99ff8b6c7
tesla lee <leechee89@gmail.com>
2013-12-13 16:53:53 +0800

    student edit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -56,6 +56,7 @@ class StudentsController < ApplicationController
   # GET /students/1/edit
   def edit
     @student = Student.find(params[:id])
+    render partial:'edit'
   end
 
   # POST /students
