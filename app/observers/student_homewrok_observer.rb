9fbe768aa6571aa8a2ed1d8db368e2deefde1f69
Charlot <7wangsong@gmail.com>
2013-12-17 18:38:38 +0800

    finish student homework observer

--- app/observers/student_homewrok_observer.rb
+++ app/observers/student_homewrok_observer.rb
@@ -1,5 +1,13 @@
 #encoding: utf-8
 class StudentHomeworkObserver<ActiveRecord::Observer
   observe :student_homewrok
-
+  def after_create student_homewrok
+    student_homewrok.homewrok.increment!(:unmark_number)
+  end
+  
+  def after_update student_homewrok
+    if student_homewrok.marked_changed?
+      student_homewrok.homewrok.decrement!(:unmark_number)
+    end
+  end
 end
