e692aaf0e8ebae937f07416635213ebd155615f8
Charlot <7wangsong@gmail.com>
2013-12-17 18:31:12 +0800

    teacher index link

--- app/presenters/teacher_course_presenter.rb
+++ app/presenters/teacher_course_presenter.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 class TeacherCoursePresenter<Presenter
-  def_delegators :@teacher_course,:course_id,:sub_course_id,:course_name,:sub_course_name,:is_default,:status,:type,:description,:start_date,:end_date,:user_id,:institution_id
+  def_delegators :@teacher_course,:id,:course_id,:sub_course_id,:course_name,:sub_course_name,:is_default,:status,:type,:description,:start_date,:end_date,:user_id,:institution_id
   def initialize(teacher_course)
     @teacher_course=teacher_course
   end
