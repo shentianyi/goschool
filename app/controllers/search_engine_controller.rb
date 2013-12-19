ec014eb2709872a1e4b23012bc8148c9e1805e15
Tianyi Shen <tianyi.shen@cz-tek.com>
2013-12-19 19:08:51 +0800

    search.js final

--- app/controllers/search_engine_controller.rb
+++ app/controllers/search_engine_controller.rb
@@ -2,6 +2,13 @@
 class SearchEngineController < ApplicationController
   skip_load_and_authorize_resource
   layout nil
+
+  #Parameters:
+  #search_type: full_text
+  #{search_type:full_text,entity_type:"Student",page:1,per_page:20,search_queries:"a string"}
+
+  #search_type: select_query
+  #{search_type:"select_query",entity_type:"Student",page:1,per_page:20,search_queries:[{query_type:"StudentName",parameters:[]}}
   def search
     SearchEngine.new.seach(params[:search_type],params[:entity_type],params[:q],params[:page])
   end
