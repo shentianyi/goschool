7a30ed9f74acb92801a46d0e9668b3f45377ac75
Charlot <7wangsong@gmail.com>
2013-12-20 10:04:41 +0800

    method

--- app/controllers/search_engine_controller.rb
+++ app/controllers/search_engine_controller.rb
@@ -6,10 +6,9 @@ class SearchEngineController < ApplicationController
   #Parameters:
   #search_type: full_text
   #{search_type:full_text,entity_type:"Student",page:1,per_page:20,search_queries:"a string"}
-
   #search_type: select_query
   #{search_type:"select_query",entity_type:"Student",page:1,per_page:20,search_queries:[{query_type:"StudentName",parameters:[]}}
   def search
-    SearchEngine.new.seach(params[:search_type],params[:entity_type],params[:q],params[:page])
+    SearchEngine.new.search(params[:search_type],params[:entity_type],params[:q],params[:page],20,current_tenant.id)
   end
 end
