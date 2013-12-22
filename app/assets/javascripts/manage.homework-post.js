c41896a64f5bce18d06b67ef4b0dc45d55b7d3b3
pptalent <wayneup@163.com>
2013-12-22 19:08:26 +0800

    homework and post

--- app/assets/javascripts/manage.homework-post.js
+++ app/assets/javascripts/manage.homework-post.js
@@ -3,6 +3,14 @@
  */
 (function(){
     $('#accordion').accordion();
+    $("body").on("click","#add-item",function(){
+        $("#homework-post-add").css("left",0).css("right",0);
+    });
+    $("body").on("click","#homework-post-add .remove",function(){
+        $("#homework-post-add input,#homework-post-add textarea").val("");
+        $("#homework-post-add").css("left","-999em").css("right","auto");
+
+    });
     $(window).resize(function(){
         var width=$("#post-home").width()-$("#post-home .left").width()-80;
         $("#post-home .right").width(width);
