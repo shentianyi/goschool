152ecba410ab16d7b213b349104a4641f9a0c52e
pptalent <wayneup@163.com>
2013-12-22 20:02:46 +0800

    homework add upload button

--- app/assets/javascripts/FRONT_homework.js
+++ app/assets/javascripts/FRONT_homework.js
@@ -1,22 +1,17 @@
 /**
- * Created with JetBrains RubyMine.
- * User: wayne
- * Date: 13-12-10
- * Time: 上午10:20
- * To change this template use File | Settings | File Templates.
+ * Created by wayne on 13-12-22.
  */
 (function(){
-    $("#main-navigator-bottom i").popup();
-    $("body").on("click","[name='upload-file']",function(event){
+    $("body").on("click","#upload-homework",function(event){
         stop_propagation(event);
         var top=$(this)[0].getBoundingClientRect().bottom;
         var left=$(this)[0].getBoundingClientRect().right-140;
         $("#upload-wrap").css("top",top).css("left",left).find("p").remove();
     }).on("click","#upload-wrap .remove",function(){
             $("#upload-wrap").css("top",0).css("left","-999em");
-    }).on("click",function(){
+        }).on("click",function(){
             $("#upload-wrap .remove").click();
-    }).on("click","#upload-wrap a,#upload-wrap,#upload-wrap p",function(event){
+        }).on("click","#upload-wrap a,#upload-wrap,#upload-wrap p",function(event){
             stop_propagation(event);
-    })
+        })
 })()
