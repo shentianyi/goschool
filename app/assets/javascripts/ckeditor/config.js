fb97ebf18838608e07896be2a9b4a5be03d5112e
Charlot <7wangsong@gmail.com>
2013-12-23 14:52:29 +0800

    menu

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -13,39 +13,26 @@
  */
 
 CKEDITOR.editorConfig = function(config) {
-	config.removeDialogTabs = 'image:advanced;link:advanced;flash:advanced;table:advanced';
-	config.toolbar = [{
-		name : 'document',
-		groups : ['mode', 'document', 'doctools'],
-		items : ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates']
-	},    '/', {
-		name : 'basicstyles',
-		groups : ['basicstyles', 'cleanup'],
-		items : ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
-	}, {
-		name : 'paragraph',
-		groups : ['list', 'indent', 'blocks', 'align', 'bidi'],
-		items : ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language']
-	}, {
-		name : 'links',
-		items : ['Link', 'Unlink', 'Anchor']
-	}, {
-		name : 'insert',
-		items : ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak']
-	}, '/', {
-		name : 'styles',
-		items : ['Styles', 'Format', 'Font', 'FontSize']
-	}, {
-		name : 'colors',
-		items : ['TextColor', 'BGColor']
-	}, {
-		name : 'tools',
-		items : ['Maximize', 'ShowBlocks']
-	}, {
-		name : 'others',
-		items : ['-']
-	}, {
-		name : 'about',
-		items : ['About']
-	}];
+     config.removeDialogTabs = 'image:advanced;link:advanced;flash:advanced;table:advanced';
+     config.toolbar = ['/', {
+          name : 'basicstyles',
+          groups : ['basicstyles', 'cleanup'],
+          items : ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
+     }, {
+          name : 'colors',
+          items : ['TextColor', 'BGColor']
+     }, {
+          name : 'paragraph',
+          groups : ['list', 'indent', 'blocks', 'align', 'bidi'],
+          items : ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language']
+     }, {
+          name : 'links',
+          items : ['Link', 'Unlink']
+     }, {
+          name : 'insert',
+          items : ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar']
+     }, '/', {
+          name : 'styles',
+          items : ['Styles', 'Format', 'Font', 'FontSize']
+     }];
 };
