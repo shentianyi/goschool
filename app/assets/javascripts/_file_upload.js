905b3836c8425cfd137fad98ca3b9a7b89d5d8d7
tesla lee <leechee89@gmail.com>
2013-12-23 17:39:46 +0800

    sd

--- app/assets/javascripts/_file_upload.js
+++ app/assets/javascripts/_file_upload.js
@@ -1,73 +1,76 @@
 //init file upload page
 function attach_upload() {
-     var vali = true;
-     var lock = false;
-     var id;
-     $(".task-attach-upload").fileupload({
-          singleFileUploads : false,
-          dataType : 'json',
-          change : function(e, data) {
-               vali = true;
-               id = $(this).attr('id');
-               $("#" + id + '-preview').show();
-               var prev = $("#" + id + '-preview');
-               $.each(data.files, function(index, file) {
-                    if(file.size < 20000000) {
-                         prev.append(
-                             $("<p/>").addClass("upload-file-name template").text("上传中....")
-                         );
-                    } else {
-                         vali = false;
-                         prev.append($("<p/>").addClass("upload-file-name").text(file.name + "&nbsp;&nbsp;" + (file.size / 1000000).toFixed(2) + "MB,超出最大值")
-                             .append($("<i />").addClass('icon remove').click(function(){$(this).parents("p").eq(0).remove()})));
-                    }
-               });
-          },
-          add : function(e, data) {
-               if(vali)
-                    if(data.submit != null)
-                         data.submit();
-          },
-          success : function(data) {
-               var prev = $("#" + id + '-preview');
-               if(data.result) {
-                   prev.find(".template").remove();
-                    for(var i = 0; i < data.object.length; i++) {
-                        prev.append(
-                            $("<p/>").addClass("upload-file-name").attr("title", data.object[i].oriName).text("上传中....")
-                        );
-                         prev.append($("<p />").addClass("attachment-item")
-                             .attr("title", data.object[i].oriName)
-                             .attr("path-name", data.object[i].pathName)
-                             .text(data.object[i].oriName)
-                             .append($("<i />").addClass("icon remove").attr("path-name", data.object[i].pathName).click(attachment_remove))
-                         )
+    var vali = true;
+    var lock = false;
+    var id;
+    $(".task-attach-upload").fileupload({
+        singleFileUploads: false,
+        dataType: 'json',
+        change: function (e, data) {
+            vali = true;
+            id = $(this).attr('id');
+            $("#" + id + '-preview').show();
+            var prev = $("#" + id + '-preview');
+            $.each(data.files, function (index, file) {
+                if (file.size < 20000000) {
+                    prev.append(
+                        $("<p/>").addClass("upload-file-name template").text("上传中....")
+                    );
+                } else {
+                    vali = false;
+                    prev.append($("<p/>").addClass("upload-file-name").text(file.name + "&nbsp;&nbsp;" + (file.size / 1000000).toFixed(2) + "MB,超出最大值")
+                        .append($("<i />").addClass('icon remove').click(function () {
+                            $(this).parents("p").eq(0).remove()
+                        })));
+                }
+            });
+        },
+        add: function (e, data) {
+            if (vali)
+                if (data.submit != null)
+                    data.submit();
+        },
+        success: function (data) {
+            //remove the previous attachments
+            var prev = $("#" + id + '-preview');
+            if (data.result) {
+                prev.find(".template").remove();
+                for (var i = 0; i < data.object.length; i++) {
+                    prev.append(
+                        $("<p/>").addClass("upload-file-name").attr("title", data.object[i].oriName).text("上传中....")
+                    );
+                    prev.append($("<p />").addClass("attachment-item")
+                        .attr("title", data.object[i].oriName)
+                        .attr("path-name", data.object[i].pathName)
+                        .text(data.object[i].oriName)
+                        .append($("<i />").addClass("icon remove").attr("path-name", data.object[i].pathName).click(attachment_remove))
+                    )
 
-                    }
-               } else {
-                    alert(data.content);
-               }
-          }
-     });
+                }
+            } else {
+                alert(data.content);
+            }
+        }
+    });
 }
 
 function attachment_remove(event) {
-     var e = event ? event : (window.event ? window.event : null);
-     var obj = e.srcElement || e.target;
-     e.stopPropagation();
-     $(".attachment-item[path-name='" + $(obj).attr("path-name") + "']").remove();
-     if($("#task-attach-uploader-preview").children().length == 0) {
-          $("#task-attach-uploader-preview").css("display", "none");
-     }
+    var e = event ? event : (window.event ? window.event : null);
+    var obj = e.srcElement || e.target;
+    e.stopPropagation();
+    $(".attachment-item[path-name='" + $(obj).attr("path-name") + "']").remove();
+    if ($("#task-attach-uploader-preview").children().length == 0) {
+        $("#task-attach-uploader-preview").css("display", "none");
+    }
 }
 
 function get_attach() {
-     var attachs = [];
-     var length = $("#task-attach-uploader-preview").children().length;
-     for(var i = 0; i < length; i++) {
-          attachs[i] = {};
-          attachs[i].oriName = $("#task-attach-uploader-preview >div").eq(i).attr("title");
-          attachs[i].pathName = $("#task-attach-uploader-preview >div").eq(i).attr("path-name")
-     }
-     return attachs;
+    var attachs = [];
+    var length = $("#task-attach-uploader-preview").children().length;
+    for (var i = 0; i < length; i++) {
+        attachs[i] = {};
+        attachs[i].oriName = $("#task-attach-uploader-preview >div").eq(i).attr("title");
+        attachs[i].pathName = $("#task-attach-uploader-preview >div").eq(i).attr("path-name")
+    }
+    return attachs;
 }
--- app/assets/javascripts/manage.post.base.js
+++ app/assets/javascripts/manage.post.base.js
@@ -10,7 +10,7 @@ function init_post() {
 function bind_menu_event() {
     $(".post-menu-a[pin=false]").click(function () {
 
-        var post_list = $(this).parent();
+        var post_list = $(this);
         if (post_list.hasClass("active")) {
             post_list.removeClass("active");
             post_list.next('div').removeClass("active");
@@ -26,6 +26,7 @@ function bind_menu_event() {
     });
 
     $('body').on('click', ".post-menu-item-a", function () {
+        var target = $(this);
         post_manager.show($(this).attr('id'), function (data) {
             $("#post-content").html(data);
         });
@@ -33,6 +34,7 @@ function bind_menu_event() {
 }
 
 function comment() {
+
     var post_id = $("#post").attr("post")
     var content = $("#comment").val();
 
@@ -48,6 +50,30 @@ function comment() {
 
         } else {
 
+        }
+    })
+}
+
+function create_post(){
+    var title = $("#title").val();
+    var content = $("#content").val();
+    var id = $("#course").attr("course");
+    var data = {
+        id:id,
+        post:{
+            title:title,
+            content:content
+        }
+    }
+    var attachs = get_attach();
+
+    data.attachs = attachs;
+
+    posts_manager.create(data,function(data){
+        if(data.result){
+            MessageBox("新建帖子成功","top","success");
+        }else{
+            MessageBox_content(data.content)
         }
     })
 }
