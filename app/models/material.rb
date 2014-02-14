712a52701c32f09b1deb077126d03870de908b5e
Charlot <7wangsong@gmail.com>
2014-02-14 14:49:04 +0800

    fix validate

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,17 +1,17 @@
 #encoding: utf-8
 class Material < ActiveRecord::Base
-  belongs_to :materialable  , :polymorphic=>true
+  belongs_to :materialable, :polymorphic => true
   belongs_to :material
   belongs_to :logininfo
-  has_many :materials,:dependent => :delete_all
+  has_many :materials, :dependent => :delete_all
 
-  attr_accessible :description, :name, :status ,:materialable_type
+  attr_accessible :description, :name, :status, :materialable_type
   acts_as_tenant(:tenant)
 
   validate :validate_save
 
   private
   def validate_save
-    errors.add(:name,'材料名称不可为空') if self.name.blank?
+    errors.add(:name, '材料名称不可为空') if self.name.blank? && (self.materialable.is_a?(Setting) || self.materialable.is_a?(Course)))
   end
 end
