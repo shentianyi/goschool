c33f0a9573ecd937d95d6d788e172f70f06382e8
Charlot <7wangsong@gmail.com>
2014-03-11 11:24:48 +0800

    fix red

--- script/init_data/create_tenant.rb
+++ script/init_data/create_tenant.rb
@@ -4,5 +4,5 @@ if ARGV.count==3
   user.create_tenant_user!(ARGV[0], ARGV[1], ARGV[1], ARGV[2])
   puts "email: #{ARGV[0]}, password: #{ARGV[1]}, company name: #{ARGV[2]}"
 else
-  puts 'params error!'.red
+  puts 'params error!'
 end
